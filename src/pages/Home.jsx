26896fe7508135fcb8fe0c3c7661298e12c16827
Santiago Lopera Naranjo <santiagoln09@gmail.com>
2021-10-20 21:24:17 -0500

    [Layout Done]

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>React Shop</h1>
+    </div>
+  )
+}
+
+export default Home
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
-import Layout from '../container/Layout';
-import Login from '../container/Login';
-import RecoveryPassword from '../container/RecoveryPassword';
-import '../styles/global.css'
-
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Layout from "../container/Layout";
+import Checkout from "../pages/Checkout";
+import CreateAccount from "../pages/CreateAccount";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import MyAccount from "../pages/MyAccount";
+import NewPassword from "../pages/NewPassword";
+import NotFound from "../pages/NotFound";
+import Orders from "../pages/Orders";
+import RecoveryPassword from "../pages/RecoveryPassword";
+import SendEmail from "../pages/SendEmail";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-				<Switch>
-						<Layout>
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/recovery-password" component={RecoveryPassword} />
-						</Layout>
-				</Switch>		
-		</BrowserRouter>
-	);
-}
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={RecoveryPassword} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
